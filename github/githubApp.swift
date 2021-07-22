6ee128a74fd39fe08d96f4cacbaafe9f37527f9a
sayed <sayed_222333@yahoo.com>
2021-07-22 13:50:42 +0300

    Initial Commit

---
+++ github/githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubApp.swift
+//  github
+//
+//  Created by MacBook Pro on 22/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
